eeee23bffea3326d12f7fcef95cef9b6862f2a9b
Matheus Wendel <matheus_wendel@outlook.com>
2020-07-01 10:15:08 -0300

    [DONE]metodos da fachada retornarão ResponseEntity

--- src/main/java/com/fatec/mogi/facade/Ifacade.java
+++ src/main/java/com/fatec/mogi/facade/Ifacade.java
@@ -2,13 +2,16 @@ package com.fatec.mogi.facade;
 
 import java.util.List;
 
+import org.springframework.http.ResponseEntity;
+
 import com.fatec.mogi.model.EntidadeDominio;
 
 public interface Ifacade {
 	
-	void save(EntidadeDominio entidade);
-	void delete(EntidadeDominio entidade);
-	List<EntidadeDominio> find(EntidadeDominio entidade);
+	ResponseEntity<EntidadeDominio> save(EntidadeDominio entidade);
+	ResponseEntity<EntidadeDominio> delete(EntidadeDominio entidade);
+	ResponseEntity<List<? extends EntidadeDominio>> find(EntidadeDominio entidade);
+	ResponseEntity<EntidadeDominio> update(EntidadeDominio entidade);
 	
 
 }
