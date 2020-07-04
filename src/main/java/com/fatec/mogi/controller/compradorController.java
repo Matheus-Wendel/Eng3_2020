52f7216329203ab97ad4f7de8651147623d07092
Matheus Wendel <matheus_wendel@outlook.com>
2020-07-04 20:18:16 -0300

    [wip]encriptacao de senha no cadastro de comprador

--- src/main/java/com/fatec/mogi/controller/compradorController.java
+++ src/main/java/com/fatec/mogi/controller/compradorController.java
@@ -1,61 +1,25 @@
 package com.fatec.mogi.controller;
 
-import java.util.List;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.DeleteMapping;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.fatec.mogi.command.CommandAtualizar;
-import com.fatec.mogi.command.CommandBuscar;
-import com.fatec.mogi.command.CommandExcluir;
-import com.fatec.mogi.command.CommandSalvar;
-import com.fatec.mogi.model.EntidadeDominio;
 import com.fatec.mogi.model.Comprador;
+import com.fatec.mogi.model.EntidadeDominio;
 
 @RestController
 @RequestMapping("/comprador")
-public class compradorController {
+public class compradorController extends AbstractController<Comprador> {
 
 	@Autowired
-	CommandSalvar commandSalvar;
-	@Autowired
-	CommandBuscar commandBuscar;
-	@Autowired
-	CommandExcluir commandExcluir;
-	@Autowired
-	CommandAtualizar commandAtualizar;
+	private BCryptPasswordEncoder bCryptPasswordEncoder;
 
-	@PostMapping
-	public ResponseEntity<EntidadeDominio> save(@RequestBody Comprador comprador) {
-		return commandSalvar.execute(comprador);
+	@Override
+	public ResponseEntity<EntidadeDominio> save(@RequestBody Comprador entidade) {
+		entidade.setSenha(bCryptPasswordEncoder.encode(entidade.getSenha()));
+		return super.save(entidade);
 	}
-
-	@GetMapping
-	public ResponseEntity<List<? extends EntidadeDominio>> find(@RequestBody(required = false) Comprador comprador) {
-		if (comprador == null) {
-			comprador = new Comprador();
-		}
-		return commandBuscar.execute(comprador);
-
-	}
-
-	@DeleteMapping
-	public ResponseEntity<EntidadeDominio> delete(@RequestBody(required = true) Comprador comprador) {
-		return commandExcluir.execute(comprador);
-
-	}
-
-	@PutMapping
-	public ResponseEntity<EntidadeDominio> update(@RequestBody(required = true) Comprador comprador) {
-		return commandAtualizar.execute(comprador);
-
-	}
-
 }
