eef0fed776a54f154638d4f8fd8ed2af7c7ec4db
Matheus Wendel <matheus_wendel@outlook.com>
2020-07-04 23:22:47 -0300

    [done] repo para todas as classes de dominio

---
+++ src/main/java/com/fatec/mogi/repository/ComponenteRepository.java
@@ -0,0 +1,9 @@
+package com.fatec.mogi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fatec.mogi.model.Componente;
+
+public interface ComponenteRepository extends JpaRepository<Componente, Integer> {
+
+}
---
+++ src/main/java/com/fatec/mogi/repository/FichaTecnicaLinhaRepository.java
@@ -0,0 +1,9 @@
+package com.fatec.mogi.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.fatec.mogi.model.FichaTecnicaLinha;
+
+public interface FichaTecnicaLinhaRepository extends JpaRepository<FichaTecnicaLinha, Integer> {
+
+}
