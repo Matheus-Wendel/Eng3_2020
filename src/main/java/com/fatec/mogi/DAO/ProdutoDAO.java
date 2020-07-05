8aba0b994f8e233c91d54d61173593b878a2ab9d
Matheus Wendel <matheus_wendel@outlook.com>
2020-07-04 23:23:26 -0300

    [done] metodo delete inativa o produto

--- src/main/java/com/fatec/mogi/DAO/ProdutoDAO.java
+++ src/main/java/com/fatec/mogi/DAO/ProdutoDAO.java
@@ -1,17 +1,44 @@
 package com.fatec.mogi.DAO;
 
+import javax.transaction.Transactional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 
+import com.fatec.mogi.model.EntidadeDominio;
 import com.fatec.mogi.model.Produto;
+import com.fatec.mogi.repository.ProdutoRepository;
 @Service
 public class ProdutoDAO extends AbstractDAO<Produto> {
 
+	ProdutoRepository  produtoRepository;
+	
 	@Autowired
 	public ProdutoDAO(JpaRepository<Produto, Integer> repo) {
 		super(repo);
+		produtoRepository = (ProdutoRepository) this.repositorio;
 	}
 
+	
+	@Override
+	@Transactional
+	public ResponseEntity<EntidadeDominio> delete(EntidadeDominio entidadeDominio) {
+		Produto entidade = (Produto) entidadeDominio;
+		try {
+			if (repositorio.existsById(entidade.getId())) {
+				produtoRepository.inativar(entidade.getId());
+				return ResponseEntity.ok().build();
+			} else {
+				return ResponseEntity.status(HttpStatus.NOT_FOUND).build();
+			}
+
+		} catch (Exception e) {
+			System.err.println(e);
+			return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).build();
+		}
+	}
 	
 }
