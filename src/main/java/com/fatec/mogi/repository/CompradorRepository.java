dddbbdce66492fd7b06bb16338b49a6ea8142139
Matheus Wendel <matheus_wendel@outlook.com>
2020-07-04 21:14:32 -0300

    [done]busca para login no repositorio comprador

--- src/main/java/com/fatec/mogi/repository/CompradorRepository.java
+++ src/main/java/com/fatec/mogi/repository/CompradorRepository.java
@@ -6,10 +6,11 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 import com.fatec.mogi.model.Comprador;
-@Repository
-public interface CompradorRepository extends JpaRepository<Comprador, Integer>  {
 
-	
-	
+@Repository
+public interface CompradorRepository extends JpaRepository<Comprador, Integer> {
+
 	List<Comprador> findByNomeContaining(String nome);
+
+	Comprador findByLogin(String login);
 }
