b6ee06321dbebe5441df809c6654f0240e93a75e
Matheus Wendel <matheus_wendel@outlook.com>
2020-07-04 23:28:24 -0300

    [done] query para desativar produtos

--- src/main/java/com/fatec/mogi/repository/ProdutoRepository.java
+++ src/main/java/com/fatec/mogi/repository/ProdutoRepository.java
@@ -3,6 +3,9 @@ package com.fatec.mogi.repository;
 import java.util.List;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import com.fatec.mogi.model.Produto;
@@ -10,5 +13,9 @@ import com.fatec.mogi.model.Produto;
 public interface ProdutoRepository extends JpaRepository<Produto, Integer> {
 
 	List<Produto> findByNomeContaining(String nome);
+	
+	@Modifying(clearAutomatically = true)
+	@Query("update Produto e set e.ativo=0 where e.id= :id")
+	public void inativar(@Param(value = "id") Integer id); 
 
 }
