eb86cd78d6028d7fab1effedc50967e64e6a369d
Matheus Wendel <matheus_wendel@outlook.com>
2020-07-04 21:15:24 -0300

    [wip] bean de encriptacao de senha

--- src/main/java/com/fatec/mogi/CompraProdutos.java
+++ src/main/java/com/fatec/mogi/CompraProdutos.java
@@ -2,13 +2,19 @@ package com.fatec.mogi;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
-@SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
+@SpringBootApplication()
 public class CompraProdutos {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CompraProdutos.class, args);
+	}
+
+	@Bean
+	public BCryptPasswordEncoder bCryptPasswordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
