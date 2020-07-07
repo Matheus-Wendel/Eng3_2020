29d4e203c2398d536603c0580daa9948ae43f4a0
Matheus Wendel <matheus_wendel@outlook.com>
2020-07-06 22:30:00 -0300

    [fix] h2 console

--- src/main/java/com/fatec/mogi/auth/WebSecurity.java
+++ src/main/java/com/fatec/mogi/auth/WebSecurity.java
@@ -26,12 +26,15 @@ public class WebSecurity extends WebSecurityConfigurerAdapter{
 	    protected void configure(HttpSecurity http) throws Exception {
 	        http.cors().and().csrf().disable().authorizeRequests()
 	                .antMatchers(HttpMethod.POST, "/comprador").permitAll()
+	                .antMatchers("/h2-console").permitAll()
+	                .antMatchers("/h2-console/**").permitAll()
 	                .anyRequest().authenticated()
 	                .and()
 	                .addFilter(new JWTAuthenticationFilter(authenticationManager()))
 	                .addFilter(new JWTAuthorizationFilter(authenticationManager()))
 	                // this disables session creation on Spring Security
 	                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+	        http.headers().frameOptions().disable();
 	    }
 
 	    @Override
